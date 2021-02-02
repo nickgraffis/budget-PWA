191f627567532e9a82bbbd5f9c535c533c7556a7
Nick Graffis <nicholasgraffis@gmail.com>
2021-02-02 15:25:24 -0800

    ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3002;
+const PORT =  process.env.PORT || 3002;
 
 const app = express();
 
@@ -15,11 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // routes
 app.use(require("./routes/api.js"));
 
